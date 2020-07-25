80a0769b74a6ee922ed37cd0db77864d8be6ed70
Tejas Gowda <tejas@syook.com>
2020-07-25 22:13:19 +0530

    prettier changes

--- expressApp.js
+++ expressApp.js
@@ -1,24 +1,24 @@
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const morgan = require("morgan");
-const path = require("path");
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const path = require('path');
 
-const router = require("./api/routes");
-const response = require("./helpers/response");
+const router = require('./api/routes');
+const response = require('./helpers/response');
 
 const app = express();
 
 // Don't send server app information in api response
-app.disable("x-powered-by");
+app.disable('x-powered-by');
 
 // Use CORS
 app.use(cors());
-app.options("*", cors());
+app.options('*', cors());
 
 // Setting up basic middleware for all Express requests
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 
 // parse request body
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,13 +26,13 @@ app.use(bodyParser.json());
 
 // Enable CORS from client-side
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials"
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials'
   );
-  res.header("Access-Control-Allow-Credentials", "true");
+  res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
@@ -40,12 +40,8 @@ router(app);
 
 // Error handler
 app.use((error, req, res, next) => {
-  console.error("error", error);
-  res
-    .status(error.status || 500)
-    .json(
-      response.error({ message: "Something went wrong. Please try again" })
-    );
+  console.error('error', error);
+  res.status(error.status || 500).json(response.error({ message: 'Something went wrong. Please try again' }));
 });
 
 module.exports = app;
--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-require("dotenv").config();
+require('dotenv').config();
 
-const expressApp = require("./expressApp");
+const expressApp = require('./expressApp');
 
-process.on("SIGINT", () => {
-  console.log("process exit on SIGINT");
+process.on('SIGINT', () => {
+  console.log('process exit on SIGINT');
   process.exit(0);
 });
 
-process.on("SIGTERM", () => {
-  console.log("process exit on SIGTERM");
+process.on('SIGTERM', () => {
+  console.log('process exit on SIGTERM');
   process.exit(1);
 });
 
-process.on("uncaughtException", error => {
-  console.error("process exit on uncaughtException", error);
+process.on('uncaughtException', error => {
+  console.error('process exit on uncaughtException', error);
   process.exit(1);
 });
 
-process.on("exit", code => {
-  console.log("process exit", code);
+process.on('exit', code => {
+  console.log('process exit', code);
 });
 
 expressApp.listen(process.env.PORT, () => {
